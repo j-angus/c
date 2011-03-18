749bb5c777f4f07b231d93aee1c97bb564f8449e
Jason Angus <jaduffa@gmail.com>
2011-03-18 23:09:36 +0800

    studying precedence

--- cpama/ch04/rev_digits.c
+++ cpama/ch04/rev_digits.c
@@ -31,9 +31,29 @@ int main (int argc, char **argv)
 			"number dude.\n");
 	}
 	else
-		printf("Digits reversed: %d", rev_int(num));
+		printf("Digits reversed: %d\n", rev_int(num));
 	
-	
+	int i, j, k;
+	i = j = k = 1;
+	i += j += k;
+	printf("i: %d, j: %d, k: %d\n", i, j, k);
+	i = 1; j = 2; k = 3;
+	i -= j -= k;
+	printf("i: %d, j: %d, k: %d\n", i, j, k);
+	i = 2; j = 1; k = 0;
+	i *= j *= k;
+	printf("i: %d, j: %d, k: %d\n", i, j, k);
+	i = 7;
+	j = 6 + (i = 2.5);
+	printf("i: %d, j: %d\n", i, j);
+	i = 2; j = 8;
+	j = (i = 6) + (j = 3);
+	printf("i: %d, j: %d\n", i, j);
+	i = 1;
+	printf("i: %d\n", i++ -1);
+	printf("i: %d\n", i);
+	i = 10; j = 5;
+	printf("%d\n", i++ - ++j);
+	printf("i: %d, j: %d\n", i, j);
 	return 0;
 }
-
