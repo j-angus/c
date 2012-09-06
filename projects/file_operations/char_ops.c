1e34ff0429fff841cefb731ebff449070c5b53cb
Jason Angus <jaduffa@gmail.com>
2012-09-07 04:45:08 +0800

    Working on read_line().

--- projects/file_operations/char_ops.c
+++ projects/file_operations/char_ops.c
@@ -25,10 +25,24 @@
  * -------
  * A pointer to the string read, NULL on error or no data.
  */
-char *read_line(FILE *src, size_t num_char)
+char *read_line(FILE *src, size_t max_char)
 {
 	printf("%s\n", __func__);
 	char *buf = NULL;
+	size_t bufsz = 2;
+	size_t num_char = 0;
+
+	if ((buf = malloc(bufsz)) == NULL) {
+		printf("ERROR in %s(): malloc failed to allocate memory.\n",
+				__func__);
+	}
+	else {
+		while (*buf = getchar(src)) {
+			if (*buf++ == '\n') {
+				break;
+			}
+			++num_char;
+			if (num_char > (bufsz - 2)) {
 
 	return buf;
 }
